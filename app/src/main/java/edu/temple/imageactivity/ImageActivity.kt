a767d4b493352afccf6a9e1fcc9f7e39410265e8
Saimon Shrestha <sai.shrestha14@gmail.com>
2021-09-28 20:02:07 -0400

    Added a grid layout and worked on ImageAdapter.kt and ImageActivity.kt

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -2,12 +2,16 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recycle = findViewById<RecyclerView>(R.id.recyclerView)
+        recycle.layoutManager= GridLayoutManager(this,4)
         val images = arrayOf(
             Image("Large Blue Note", R.drawable.largebluenote),
             Image("Large Green Note", R.drawable.largegreennote),
@@ -20,10 +24,11 @@ class MainActivity : AppCompatActivity() {
             Image("Medium Purple Note", R.drawable.mediumbluenote),
             Image("Medium Red Note", R.drawable.mediumbluenote),
             Image("Small Blue Note", R.drawable.smallbluenote),
-            Image("Small Green Note", R.drawable.smallbluenote),
-            Image("Small Orange Note", R.drawable.smallbluenote),
-            Image("Small Purple Note", R.drawable.smallbluenote),
-            Image("Small Red Note", R.drawable.smallbluenote)
+            Image("Small Green Note", R.drawable.smallgreenote),
+            Image("Small Orange Note", R.drawable.smallorangenote),
+            Image("Small Purple Note", R.drawable.smallpurplenote),
+            Image("Small Red Note", R.drawable.smallrednote)
         )
+        recycle.adapter=ImageAdapter(this,images)
     }
 }
--- app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
@@ -1,24 +1,33 @@
 package edu.temple.imageactivity
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter(): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
-    class ImageViewHolder(_imageView: ImageView): RecyclerView.ViewHolder(_imageView){
-        val imageView = _imageView
+class ImageAdapter(private val context: Context, private val imageObj: Array<Image>): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+    class ImageViewHolder(_imageView: View): RecyclerView.ViewHolder(_imageView){
+        val noteImg: ImageView = _imageView.findViewById(R.id.noteImg)
+        val noteDesc: TextView = _imageView.findViewById(R.id.noteDesc)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val imageView = ImageView(parent.context)
+        val imageView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.grid_layout,parent,false)
         return ImageViewHolder(imageView)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.imageView
+        val img = imageObj[position]
+        holder.noteImg.setImageDrawable(AppCompatResources.getDrawable(context,img.resourceId))
+        holder.noteDesc.text=img.desc
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return imageObj.size
     }
 }
